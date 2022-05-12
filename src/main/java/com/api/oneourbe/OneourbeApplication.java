2c4f973168a61743b1723398b9b8104e957112de
januarry22 <tls2330@naver.com>
2022-05-12 18:07:03 +0900

    Initial Commit

---
+++ src/main/java/com/api/oneourbe/OneourbeApplication.java
@@ -0,0 +1,13 @@
+package com.api.oneourbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneourbeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OneourbeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/oneourbe/OneourbeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.oneourbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneourbeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
