bbdcf6ff5ac6620aaedc4c76943bd0f7a5790086
januarry22 <tls2330@naver.com>
2023-02-09 23:04:33 +0900

    [ADD] : 유저 인증서버 Util 추가

--- src/main/java/com/api/oneourbe/util/security/SecurityUtil.java
+++ src/main/java/com/api/oneourbe/util/security/SecurityUtil.java
@@ -1,4 +1,4 @@
-package com.api.oneourbe.util;
+package com.api.oneourbe.util.security;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.gson.Gson;
@@ -10,48 +10,10 @@ import org.springframework.stereotype.Component;
 import java.util.Map;
 
 @Component
-public class ApiPrintUtil {
+public class SecurityUtil {
 
-    static Gson gson = new GsonBuilder().setPrettyPrinting().serializeNulls().create();
+    public Object oAuth(){
 
-    public static void reqPrint(String str){
-        try{
-            JsonParser jp = new JsonParser();
-            JsonElement je = jp.parse(str);
-            str = gson.toJson(je);
-            System.out.println("############################################      Request(요청) Param     #################################");
-            System.out.println(str);
-            System.out.println("############################################      Request(요청) Param     #################################");
-            //    this.reqSave(str);
-        }catch(Exception e){
-            //errorCode.save(e);
-        }
-    }
-    public static void returnPrint(String str){
-        try{
-            JsonParser jp = new JsonParser();
-            JsonElement je = jp.parse(str);
-            str = gson.toJson(je);
-
-            System.out.println("############################################      Response(응답) Param     ################################");
-            System.out.println(str);
-            System.out.println("############################################      Response(응답) Param     ################################");
-            //  this.resSave(str);
-        }catch(Exception e){
-            //errorCode.save(e);
-        }
-    }
-
-    public static String mapToJson(Object obj){
-        try{
-            String ss = "";
-            ObjectMapper objectMapper = new ObjectMapper();
-            Map map = objectMapper.convertValue(obj, Map.class);
-            ss += gson.toJson(map);
-            return ss;
-        }catch(Exception e){
-            return null;
-        }
-
+        return null;
     }
 }
